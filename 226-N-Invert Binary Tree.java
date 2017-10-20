18a6b6b5f767f502935a359d7f43459088fcd4c7
Sunshinevvv <sunshine.vvv@163.com>
2017-10-21 00:51:35 +0800

    233

---
+++ 226-N-Invert Binary Tree.java
@@ -0,0 +1,45 @@
+/**
+ * 反转二叉树
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    // 1.recursive
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    // 2.BFS
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # MyLeetCode
 My Java solutions for problems in https://leetcode.com/  
-Annotations: E(Easy) N(Normal) H(Hard)  VH(Very Hard)
-
-## Bit
-* 190-Reverse Bits : E
-* 191-Number of 1 Bits : E
-
-## String
-* 7-Reverse Integer : E
+Annotations: E(Easy) N(Normal) H(Hard)
