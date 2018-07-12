d0920fab2125d2cb7a07c546deb23fe4b75dfef0
杨光 <yangguang01@weidian.com>
2018-07-12 20:45:24 +0800

    441. Arranging Coins

--- 441. Arranging Coins.java
+++ 441. Arranging Coins.java
@@ -1,11 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
- * playground
+ You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+ Given n, find the total number of full staircase rows that can be formed.
+
+ n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+ Example 1:
+
+ n = 5
+
+ The coins can form the following rows:
+ ¤
+ ¤ ¤
+ ¤ ¤
+
+ Because the 3rd row is incomplete, we return 2.
+ Example 2:
+
+ n = 8
+
+ The coins can form the following rows:
+ ¤
+ ¤ ¤
+ ¤ ¤ ¤
+ ¤ ¤
+
+ Because the 4th row is incomplete, we return 3.
+
  */
 class Solution {
+    /**
+     * 主要纠结的点其实是如如何避免整形溢出，采取了使用long和 *0.5 两种方式
+     */
     public int arrangeCoins(int n) {
         long low = 0, high = (long) (Math.sqrt(n) * Math.sqrt(2));
         while (low <= high) {
@@ -36,22 +62,5 @@ class Solution {
             }
         }
         return low - 1;
-    }
-}
-
-public class MainClass {
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(MainClass.class.getResourceAsStream("input.txt")));
-        String line;
-        while ((line = in.readLine()) != null) {
-            int[] numbers = Utils.stringToIntegerArray(line);
-            line = in.readLine();
-            int target = Integer.parseInt(line);
-        }
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Math.sqrt(Integer.MAX_VALUE));
-        System.out.println(1.0 - 0.9);
-        int ret = new Solution().arrangeCoinsV2(1804289383);
-        System.out.print(ret);
     }
 }
